c0f22a1e887efb89b4c73698015bce7025629c5a
Andrew Sutton <andrew.n.sutton@gmail.com>
2016-01-16 10:05:27 -0500

    Add missing unit test.

---
+++ beaker/test/test_initialize.cpp
@@ -0,0 +1,42 @@
+// Copyright (c) 2015-2016 Andrew Sutton
+// All rights reserved
+
+#include "test.hpp"
+
+#include <beaker/initialization.hpp>
+
+#include <iostream>
+#include <iomanip>
+
+
+using namespace lingo;
+using namespace beaker;
+
+
+void
+test_init(Context& cxt)
+{
+  Builder build(cxt);
+
+  Type& i32 = build.get_int_type();
+  Type& r_i32 = build.get_reference_type(i32);
+  Expr& zero = build.get_zero(i32);
+
+  Variable_decl& v1 = build.make_variable("v1", i32);
+  Expr& r_v1 = build.make_reference(v1);
+
+  Init& init1 = initialize(cxt, i32, build.make_equal_init(zero));
+  std::cout << init1 << '\n';
+
+  Init& init2 = initialize(cxt, r_i32, build.make_equal_init(r_v1));
+  std::cout << init2 << '\n';
+}
+
+
+int
+main(int argc, char* argv[])
+{
+  Context c;
+
+  test_init(c);
+}
