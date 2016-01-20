54840d50a370148b240fed68bef7ae25e8fcd6ee
Andrew Sutton <andrew.n.sutton@gmail.com>
2016-01-20 10:31:53 -0500

    Stubbing out search pattern for unqualified lookup.

--- banjo/lookup.cpp
+++ banjo/lookup.cpp
@@ -8,15 +8,61 @@
 namespace banjo
 {
 
+// For convenience.
+using Binding = Scope::Binding;
+
+
+// -------------------------------------------------------------------------- //
+// Lookup rules
+
 
 // Returns the set of declarations for give (unqualified) id.
+//
+// Lookup ends as soon as a declaration is found
+// for the given name.
+//
+// TODO: How should we handle non-simple id's like operator-ids
+// and conversion function ids.
 Decl_list
 unqualified_lookup(Scope& scope, Simple_id const& id)
 {
   Scope* p = &scope;
   while (p) {
-    // Overload_set* ovl = scope->look
-    // p = p->parent(); 
+    // In general, a name used in any context must be declared 
+    // before it's use. Search this scope for such a declaration.
+    if (Binding* b = p->lookup(id.symbol()))
+      return b->second;
+
+    // Depending on current scope, we might re-direct the scope
+    // to search different things.
+
+    if (Function_scope* s = as<Function_scope>(p)) {
+      Function_decl& f = s->declaration();
+      (void)f;
+
+      // TODO: If fn is defined by a qualified-id, then we should
+      // should search the scope(s) named in the id before resuming
+      // the search.
+    }
+
+    else if (Class_scope* cs = as<Class_scope>(p)) {
+      Class_decl& c = cs->declaration();
+      (void)c;
+
+      // TODO: Include base classes of c. Lookup also depends on
+      // the declarative region of c (nested classes, locals, etc).
+    }
+
+
+    else if (Initializer_scope* s = as<Initializer_scope>(p)) {
+      Object_decl& v = s->declaration();
+      (void)v;
+
+      // TODO: If v is declared by a qualified-id, then re-direct
+      // to the scope of v before working outwards.
+    }
+
+    p = p->enclosing_scope();
   }
   return {};
 }
--- banjo/lookup.hpp
+++ banjo/lookup.hpp
@@ -7,6 +7,7 @@
 #include "prelude.hpp"
 #include "overload.hpp"
 #include "context.hpp"
+#include "ast.hpp"
 
 #include <lingo/environment.hpp>
 
@@ -15,35 +16,192 @@ namespace banjo
 {
 
 
+// -------------------------------------------------------------------------- //
+// Scope definitions
+//
+// TODO: Do I need derived kinds of scopes?
+
+
 // A scope defines a maximal lexical region of text where an 
 // entity  may be referred to without qualification. A scope can 
 // be (but is not always) associated with a declaration.
 struct Scope : Environment<Symbol const*, Overload_set>
 {
+  using Base = Environment<Symbol const*, Overload_set>;
+
   Scope()
-    : decl(nullptr)
+    : parent(nullptr), decl(nullptr)
   { }
 
-  Scope(Decl* d)
-    : decl(d)
+  Scope(Scope& p)
+    : parent(&p), decl(nullptr)
   { }
 
-  Decl* context() const { return decl; }
-  Decl* context()       { return decl; }
+  Scope(Scope& p, Decl& d)
+    : parent(&p), decl(&d)
+  { }
 
-  Decl* decl;
+  virtual ~Scope() { }
+
+  // Returns the enclosing scope, if any. Only the global
+  // namespace does not have an enclosing scope.
+  Scope const* enclosing_scope() const { return parent; }
+  Scope*       enclosing_scope()       { return parent; }
+
+  // Returns the declaration associated with the scope.
+  // Not all scopes are associated with a declaration
+  // (e.g., block scope).
+  Decl const* context() const { return decl; }
+  Decl*       context()       { return decl; }
+
+  // Return the binding for the given symbol, or nullptr
+  // if no such binding exists.
+  Binding const* lookup(Symbol const& s) const { return Base::lookup(&s); }
+  Binding*       lookup(Symbol const& s)       { return Base::lookup(&s); }
+
+  Scope* parent;
+  Decl*  decl;
 };
 
 
-// The scope stack maintains the current scope during elaboration.
-struct Scope_stack : Stack<Scope>
+// Represents a namespace scope.
+struct Namespace_scope : Scope
 {
-  Scope const& current() const { return top(); }
-  Scope&       current()       { return top(); }
-
-  Scope const& global() const { return bottom(); }
-  Scope&       global()       { return bottom(); }
+  // Returns the namespace declaration associated with the scope.
+  Namespace_decl const& declaration() const;
+  Namespace_decl&       declaration();
 };
+
+
+
+// Represents function scope.
+struct Function_scope : Scope
+{
+  // Returns the function declaration associated with the scope.
+  Function_decl const& declaration() const;
+  Function_decl&       declaration();
+};
+
+
+// Represents a class scope.
+struct Class_scope : Scope
+{
+  // Returns the function declaration associated with the scope.
+  Class_decl const& declaration() const;
+  Class_decl&       declaration();
+};
+
+
+// Represents the scope of a variables initializer. This is
+// primarily used to support lookup of identifiers when the
+// variable is declared with a qualified-id (i.e., a static
+// member of a class or a namespace variable defined outside
+// of the namespace.
+struct Initializer_scope : Scope
+{
+  // Returns the function declaration associated with the scope.
+  Object_decl const& declaration() const;
+  Object_decl&       declaration();
+};
+
+
+// Represents a block scope.
+struct Block_scope : Scope
+{
+};
+
+
+
+// TODO: Define other kinds of scope.
+
+
+inline Namespace_decl const& 
+Namespace_scope::declaration() const 
+{ 
+  return *cast<Namespace_decl>(context()); 
+}
+
+Namespace_decl&
+Namespace_scope::declaration()
+{ 
+  return *cast<Namespace_decl>(context()); 
+}
+
+
+inline Function_decl const&
+Function_scope::declaration() const
+{ 
+  return *cast<Function_decl>(context());
+}
+
+
+inline Function_decl&      
+Function_scope::declaration()      
+{ 
+  return *cast<Function_decl>(context());
+}
+
+
+inline Class_decl const&
+Class_scope::declaration() const
+{ 
+  return *cast<Class_decl>(context());
+}
+
+
+inline Class_decl&      
+Class_scope::declaration()      
+{ 
+  return *cast<Class_decl>(context());
+}
+
+
+inline Object_decl const&
+Initializer_scope::declaration() const
+{ 
+  return *cast<Object_decl>(context());
+}
+
+
+inline Object_decl&      
+Initializer_scope::declaration()      
+{ 
+  return *cast<Object_decl>(context());
+}
+
+
+
+// Returns true if s is a scope for a namespace.
+inline bool
+is_namespace_scope(Scope const& s)
+{
+  return is<Namespace_scope>(&s);
+}
+
+
+// Returns true if s is a scope for a function definition.
+inline bool
+is_function_scope(Scope const& s)
+{
+  return is<Function_scope>(&s);
+}
+
+
+// -------------------------------------------------------------------------- //
+// Scope definitions
+
+
+// The scope stack maintains the current scope for purposes
+// of unqualified lookup.
+struct Scope_stack
+{
+  Scope* curr;
+};
+
+
+
+// -------------------------------------------------------------------------- //
+// Lookup
 
 
 Decl_list unqualified_lookup(Scope_stack&, Simple_id const&);
